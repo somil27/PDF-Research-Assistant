1f772f1cddb589893691349137a3e7890d813842
Somil Shankar Gupta <itzmesomil@gmail.com>
2026-05-09 00:33:34 +0530

    Add next-themes provider

---
+++ frontend/app/theme-provider.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { ReactNode } from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+      {children}
+    </NextThemesProvider>
+  )
+}
